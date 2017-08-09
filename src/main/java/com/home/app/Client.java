e23a79d46e66cb8866e78513f1e35cc3ee10c391
ytzhangFTD <401885536@qq.com>
2017-08-09 23:27:33 +0800

    update

---
+++ src/main/java/com/home/app/Client.java
@@ -0,0 +1,20 @@
+package com.home.app;
+
+/**
+ * Administrator 2017/8/8
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        try {
+            MyThread myThread = new MyThread();
+            Thread thread=new Thread(myThread);
+            thread.start();
+            Thread.sleep(2000);
+            thread.interrupt();
+        } catch (InterruptedException e) {
+            System.out.println("main catch ");
+            e.printStackTrace();
+        }
+    }
+}
